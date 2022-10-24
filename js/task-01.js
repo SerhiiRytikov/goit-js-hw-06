45ef0fafc07b52ab02f678aef7ef50449ad53c91
SerhiiRytikov <serg_ritikov@ukr.net>
2022-10-24 16:40:32 +0300

    Робе задачу №1

---
+++ js/task-01.js
@@ -0,0 +1,27 @@
+// Напиши скрипт, який:
+
+// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного элемента li.item у спику ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+// В результаті, в консолі будуть виведені наступні повідомлення.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+const totalcategories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${totalcategories.length}`);
+
+const categoriesArray = [...totalcategories]
+  .map(
+    categories => `Category: ${categories.children[0].textContent}
+Elements: ${categories.children[1].children.length}`
+  )
+  .join("\n");
+console.log(categoriesArray);
