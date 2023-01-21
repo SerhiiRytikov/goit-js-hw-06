9313ac63a30f050976aef327986ea0304f1d66ab
SerhiiRytikov <serg_ritikov@ukr.net>
2023-01-21 21:26:43 +0200

    1

--- js/task-05.js
+++ js/task-05.js
@@ -4,10 +4,10 @@ const inputSpan = document.querySelector("#name-output");
 
 input.addEventListener("input", (event) => {
     event.target.value;
-    console.log(event.target.value);
+    console.log(event.currentTarget.value);
     if (input.value === "") {
         inputSpan.textContent = "Anonymous";
-    } else {inputSpan.textContent = event.target.value;
+    } else {inputSpan.textContent = event.currentTarget.value;
 
     }
 
