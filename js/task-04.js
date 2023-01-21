1c27e0018489f4f49608cf2287cf0c41164b68be
SerhiiRytikov <serg_ritikov@ukr.net>
2023-01-21 19:46:17 +0200

    1

--- js/task-04.js
+++ js/task-04.js
@@ -1,21 +1,23 @@
-const minus = document.querySelector("[data-action='decrement']");
-const plus = document.querySelector("[data-action='increment']");
-const span=  document.querySelector("#value");
+const refs = {
+    minus: document.querySelector("[data-action='decrement']"),
+    plus: document.querySelector("[data-action='increment']"),
+    span: document.querySelector("#value"),
+  };
   
   let counterValue = 0;
   const decrement = () => {
     counterValue -= 1;
   
-    span.textContent = counterValue;
+    refs.span.textContent = counterValue;
   };
 
     const increment = () => {
     counterValue += 1;
   
-   span.textContent = counterValue;
+   refs.span.textContent = counterValue;
   };
   
-  minus.addEventListener("click", decrement);
-  plus.addEventListener("click", increment);
+  refs.minus.addEventListener("click", decrement);
+  refs.plus.addEventListener("click", increment);
   
 
