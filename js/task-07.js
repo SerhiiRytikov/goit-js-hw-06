042d46a55cecc7200eab8f7fbf9ebf0b39b71d1f
SerhiiRytikov <serg_ritikov@ukr.net>
2023-01-23 00:16:08 +0200

    1

--- js/task-07.js
+++ js/task-07.js
@@ -10,8 +10,7 @@
 const inputRange = document.querySelector("#font-size-control");
 const inputText = document.querySelector("#text");
 
-const handleInput = () => {
+const handleInput = () => 
   inputText.style.fontSize = `${inputRange.value}px`;
-};
 
 inputRange.addEventListener("input", handleInput);
